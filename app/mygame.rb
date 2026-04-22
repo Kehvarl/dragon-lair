6e7074fb7574def36f4051944fe7dea4b0deeeeb
kehvarl <kehvarl@kehvarl.com>
2026-04-21 21:03:59 -0700

    Smarter gem hint.

--- app/mygame.rb
+++ app/mygame.rb
@@ -72,7 +72,8 @@ class MyGame < Game
         @buttons[:add_alcove].location =  [:hoard]
         highlight_button :add_alcove, 100
 
-        create_unlock :gems         # Start finding gems after... ?
+        create_unlock :gem_hit      # Tantalize the dragon when the hoard is big enough
+        create_unlock :gems         # Start finding gems after hoard is about 20 unused spaces
         create_unlock :artifacts    # Start finding artifacts after ?
 
         create_unlock :expand_hoard # When we've first dug out enough space for more gold storage
@@ -169,9 +170,10 @@ class MyGame < Game
         end
         # size > special:  Unlock special rooms (library unlocks Tomes, Museum unlocks artifacts)
 
-        if size > 15 and not unlocked?(:gems)
-            add_message :log, "The stone ahead rings melodically beneath your claws."
+        if size > 15 and not unlocked?(:gem_hint)
+          unlock(:gem_hint)
         end
+
         if size > 20 and not unlocked?(:gems)
           unlock(:gems)
         end
@@ -200,6 +202,10 @@ class MyGame < Game
 
     def check_reputation
         # Any triggers based of the dragon's reputation (maybe some follower types)
+    end
+
+    def gem_hint_unlocked
+      add_message :log, "The stone ahead rings melodically beneath your claws."
     end
 
     def gems_unlocked
